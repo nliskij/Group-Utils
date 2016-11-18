4790872daa0b3136eff0a7df6a655446c04467cf
nliskij <nliskij@users.noreply.github.com>
2016-11-17 18:42:08 -0800

    Cleaned up set comprehensions

--- groups.py
+++ groups.py
@@ -10,15 +10,24 @@ class Group:
                 return pair[0] if pair[0] != elem else pair[1]
 
     def map(self, morph):
-        return [x for x in map(morph, self.elems)]
+        return {x for x in map(morph, self.elems)}
+
+    def lcoset(self, x, sub):
+        return sub.map(lambda g: self.rel[x, g])
+
+    def rcoset(self, x, sub):
+        return sub.map(lambda g: self.rel[g, x])
+
+    def automorph(self, g):
+        return self.map(lambda h: self.rel[g, self.rel[h, self.inv(g)]])
 
     def __iter__(self):
 # I think I should've just made this inherit from set and dict
         return iter(self.elems)
 
 def cyclic(order):
-    return Group({pair : (pair[0] + pair[1]) % order for pair in [(a, b) for a
-        in range(0, order) for b in range(0, order)]}, set(range(0, order)), identity=0)
+    return Group({(a, b): (a + b) % order for a in range(0, order) for b in
+        range(0, order)}, set(range(0, order)), identity=0)
 
 def load(path):
     f = open(path, 'r')
@@ -37,3 +46,5 @@ def product(G, H):
     elems = {(g, h) for g in G for h in H}
     rel = {(a, b): (G.rel[a[0], b[0]], H.rel[a[1], b[1]]) for a in elems for b in elems}
     return Group(rel, elems, identity=(G.id, H.id))
+
+klein = product(cyclic(2), cyclic(2))
